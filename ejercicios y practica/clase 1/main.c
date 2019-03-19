167b260c41f8d8e1a1474526027254a7b2ee29be
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-03-18 23:20:11 -0300

    E/S 1

---
+++ ejercicios y practica/clase 1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char main()
+{
+    char texto ;
+    printf("Ingrese un texto: %s");
+    scanf ("%s", texto);
+    printf ("La palabra ingresada es %s", texto);
+
+    return 0;
+}
