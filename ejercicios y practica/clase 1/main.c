d9f2d8efcbb8e2afe9e88815a58690bd2d908de9
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-03-19 09:41:56 -0300

    REVISION DEL EJERCICIO

--- ejercicios y practica/clase 1/main.c
+++ ejercicios y practica/clase 1/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char main()
+int main()
 {
-    char texto ;
-    printf("Ingrese un texto: %s");
-    scanf ("%s", texto);
-    printf ("La palabra ingresada es %s", texto);
+    char texto; //LEE UN SOLO CARACTER PORQUE EL TIPO DE VARIABLE ES CHAR.
+    char palabra [16]; //siempre hay que declarar un byte mas de lo que se necesita usar. ESTO FUNCIONA COMO UN MAXIMO DE CARACTERES A INGRESAR.
+
+    printf("Ingrese un texto: ");
+    scanf ("%s", palabra); //EL ARRAY, LAS CADENAS DE PALABRAS NO NECESITAN EL & PARA MOSTRAR LA DIRECCION.
+    printf ("La palabra ingresada es %s", palabra);
 
     return 0;
 }
