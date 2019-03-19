c8fc4a138e40c7d000704792cdc99006125bc40b
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-03-19 12:17:10 -0300

    EJERCICIO DE PRACTICA CLASE 2 SIN TERMINAR.

--- ejercicios y practica/clase 2/main.c
+++ ejercicios y practica/clase 2/main.c
@@ -1,54 +1,40 @@
+/*
+Se ingresan numeros enteros (distintos de cero) hasta que el usuario quiera.
+Informar:
+
+a)cantidad de numeros ingresados.
+b)cantidad de pares.// validar si es un numero par, y calcular la cantidad.
+c)suma de los positivos. //validar si es positivo y sumarlos.
+d)promedio de los negativos. //validar si es negativo y sacar la cantidad para dividir para el promedio.
+e)maximo y minimo. //flags
+f)el mas chico de los positivos. //flags
+g)el mas grande de los negativos. //flags
+h)cantidad de numeros entre el -10 y el 50. //contador.
+e)promedio total. //contador total de numeros ingresados y suma de todos, y dividir.
+
+NO HACE FALTA VALIDAR QUE SEA UN NUMERO ENTERO, HAY QUE DECLARAR QUE ES ENTERO EN LA VARIABLE.
+
+si quiero usar una bandera con el if utilizo el 1 (true) y el 0 (verdadero). El "default" es verdadero.
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
-/*WHILE y do while --> para cuando no se sabe la cantidad de iteraciones que se lleva a cabo.
+
 int main()
 {
-   int i=0;
-    while(i<10)
-    {
-        printf("El numero es %d", i);//SI QUIERE MOSTRAR DEL UNO AL DIEZ printf("El numero es %d", i+1);
-        // el \n es como un enter, pero el \t es una tabulacion, ingresa espacios.. el \a hace sonar el speaker!
-        printf("\n Este es el valor de i: %d", i);
-        i++;
-    }
-    return=0;
-}*/
-//FOR --> Para una cantidad determinada de iteraciones.
-int main()
-{
-    int i;
-    for (i=0;i<10;i++)
-    {
-        printf("El numero es %d", i+1);
-
-    }
-
-    /*FOR INFINITO
-    1)
-    int i=10;
-    for (;;)
-    {
-        printf("El numero es %d", i);
-        i--;
-        if(i==2)
-        {
-            break;
-        }
-    2)
-    for (;;)
-    {
-        printf("El numero es %d", i);
-        i--;
-        if(i==2)
-        {
-            continue; //NO MUESTRA EL 2 SINO QUE CONTINUA CON LA SIGUIENTE ITERACION.
-        }
+    int numeroIngresado;
+    int cantidadDePares;
+    int sumaPositivos;
+    int promedioNegativos;
+    int numeroMaximo;
+    int numeroMinimo;
+    int menorNumeroPositivo;
+    int mayorNumeroNegativo;
+    int cantidadNumerosEntreMenosDiezYCincuenta;
+    int promedioTotal;
 
 
-
-        */
-
-
-
-    return=0;
+    return 0;//NO ELIMINAR.
 }
