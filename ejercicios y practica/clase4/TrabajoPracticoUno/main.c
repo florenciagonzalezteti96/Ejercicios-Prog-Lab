428a774e663b0fd73d17b7d67bc8c86145f0d8f6
florenciagonzalezteti96 <florenciagt.uni@gmail.com>
2019-03-26 10:48:39 -0300

    Inicio del TP1

---
+++ ejercicios y practica/clase4/TrabajoPracticoUno/main.c
@@ -0,0 +1,37 @@
+/*
+Hacer una calculadora. Para ello el programa iniciará y contará con un menú de opciones: (crear una funcion para el menu de opciones, utilizar un switch)
+1. Ingresar 1er operando (A=x)//crear funcion para ingresar operandos
+
+2. Ingresar 2do operando (B=y)//crear funcion para ingresar operandos
+
+3. Calcular todas las operaciones
+a) Calcular la suma (A+B)//funcion suma
+b) Calcular la resta (A-B)//funcion resta
+c) Calcular la division (A/B)//funcion division
+d) Calcular la multiplicacion (A*B)//funcion multiplicacion
+e) Calcular el factorial (A!)//funcion de calculo de factorial del primer operando
+
+4. Informar resultados //print f
+a) “El resultado de A+B es: r”
+b) “El resultado de A-B es: r”
+c) “El resultado de A/B es: r” o “No es posible dividir por cero”
+d) “El resultado de A*B es: r”
+e) “El factorial de A es: r1 y El factorial de B es: r2”
+
+5. Salir
+• Todas las funciones matemáticas del menú se deberán realizar en una biblioteca aparte,
+que contenga las funciones para realizar las cinco operaciones.
+• En el menú deberán aparecer los valores actuales cargados en los operandos A y B
+(donde dice “x” e “y” en el ejemplo, se debe mostrar el número cargado)
+• Deberán contemplarse los casos de error (división por cero, etc)
+• Documentar todas las funciones
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
