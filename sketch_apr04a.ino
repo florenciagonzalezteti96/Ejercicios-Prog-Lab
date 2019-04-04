6db654c2424b81a1bf357a02b81432d03942e296
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-04-04 12:21:36 -0300

    arduino

---
+++ sketch_apr04a.ino
@@ -0,0 +1,21 @@
+#define LED 11
+
+void setup() {
+  pinMode(LED,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED,HIGH);//selecciono el pin para usar
+  delay(1000);//para por un segundo
+
+  digitalWrite(LED,LOW);//selecciono el pin para usar
+  delay(1000);//para por un segundo
+}
+
+void titilar (int x, int vecesQueTitila, int tiempoQueTitila)
+{
+  
+}
+
